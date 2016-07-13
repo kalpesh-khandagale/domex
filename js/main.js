9695eac8f4289f9daa8406ad7b226405e37296f8
macbook <macbook@macbooks-MacBook-Pro.local>
2016-07-13 18:13:44 +0530

    Some Changes has been done

--- js/main.js
+++ js/main.js
@@ -12,3 +12,31 @@
 			updateSliderMargin();
 		}
 	});
+
+
+// menu slide toggle
+
+$(document).ready(function(){
+	var switch_icon = true;
+	$('.sw-sm').click(function(){
+		$(this).find('.sub-menu').slideToggle();
+		var temp = $(this).find('.icon-change span').html();
+		if(temp == "-"){
+			$(this).find('.icon-change span').html("+");	
+		}else{
+			$(this).find('.icon-change span').html("-");
+		}
+	});
+
+
+	$('.dis-edu').click(function(){
+		$('.dis-edu-vw').fadeIn();
+		$('.toi-trng-vw').fadeOut();
+	});
+
+	$('.toi-trng').click(function(){
+		$('.toi-trng-vw').fadeIn();
+		$('.dis-edu-vw').fadeOut();
+	});
+
+});
